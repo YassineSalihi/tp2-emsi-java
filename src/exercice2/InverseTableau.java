8673e9c5a13160c834cd4851899995b9f10d3100
YassineSalihi <y.salihi7457@uca.ac.ma>
2025-10-14 16:56:41 +0100

    to complete invverse

---
+++ src/exercice2/InverseTableau.java
@@ -0,0 +1,24 @@
+package exercice2;
+
+import java.util.*;
+
+public class InverseTableau {
+    static void main() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Entrer la taille du tableau : ");
+        int taille = sc.nextInt();
+        int[] nombre;
+        for (int i = 0; i < taille; i++){
+            System.out.println("Enter element : " + i + "#");
+
+        }
+
+
+
+
+
+
+    }
+}
